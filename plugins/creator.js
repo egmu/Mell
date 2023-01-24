397f895378632f1235b091302bc4e795214d4996
egmu <mkh.yusuf@gmail.com>
2023-01-25 05:55:17 +0700

    WA

---
+++ plugins/creator.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let pp = await conn.profilePictureUrl(who).catch(_ => hwaifu.getRandom())
+let name = await conn.getName(who)
+
+if (command == 'creator') {
+ let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp; Matchaalamu\nNICKNAME:👑 Owner Amelia Bot\nORG: Matchaalamu\nTITLE:soft\nitem1.TEL;waid=6289519240209:+62 895-1924-0209\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://youtube.com/@DEVIL-X-CODE\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: fakeanime272@gmail.com\nitem3.X-ABLabel:💌 Mail Owner AmeliaBot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 ,02 Fabruari 1000\nEND:VCARD`
+const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
+let caption = `👋 Hai *@${who.split("@")[0]}*, Nih Owner Saya kak`
+    await conn.sendButton(m.chat, caption, author, null, [['👨Sapa Owner', 'Huuu']], m, { quoted: fkontak, mentions: conn.parseMention(caption) })
+}
+if (command == 'own') {
+  let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;${author};;;\nFN:${author}\nORG:${author}\nTITLE:\nitem1.TEL;waid=6289519240209:+62 895-1924-0209\nitem1.X-ABLabel:${author}\nX-WA-BIZ-DESCRIPTION:${htjava} Nih pengembang ku kack yg mengaktifkan aq.\nX-WA-BIZ-NAME:${author}\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkontak })
+await conn.reply(m.chat, `Halo kak @${m.sender.split(`@`)[0]} itu nomor ownerku , jangan di spam ya `,m)
+  }
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(creator|own)$/i
+
+export default handler
